3edf913a19293895b801f80e0640c3a841a191f8
Fabrice Desré <fabrice@desre.org>
2022-06-29 11:40:26 -0700

    Update clap to 3.2

--- crates/arti/src/lib.rs
+++ crates/arti/src/lib.rs
@@ -325,18 +325,18 @@ pub fn main_main() -> Result<()> {
             .usage("arti <SUBCOMMAND> [OPTIONS]")
             .arg(
                 Arg::with_name("config-files")
-                    .short("c")
+                    .short('c')
                     .long("config")
                     .takes_value(true)
                     .value_name("FILE")
                     .multiple(true)
                     // NOTE: don't forget the `global` flag on all arguments declared at this level!
                     .global(true)
-                    .help(&config_file_help),
+                    .help(&*config_file_help),
             )
             .arg(
                 Arg::with_name("option")
-                    .short("o")
+                    .short('o')
                     .takes_value(true)
                     .value_name("KEY=VALUE")
                     .multiple(true)
@@ -345,7 +345,7 @@ pub fn main_main() -> Result<()> {
             )
             .arg(
                 Arg::with_name("loglevel")
-                    .short("l")
+                    .short('l')
                     .long("log-level")
                     .global(true)
                     .takes_value(true)
@@ -367,14 +367,14 @@ pub fn main_main() -> Result<()> {
                     )
                     .arg(
                         Arg::with_name("socks-port")
-                            .short("p")
+                            .short('p')
                             .takes_value(true)
                             .value_name("PORT")
                             .help("Port to listen on for SOCKS connections (overrides the port in the config if specified).")
                     )
                     .arg(
                         Arg::with_name("dns-port")
-                            .short("d")
+                            .short('d')
                             .takes_value(true)
                             .value_name("PORT")
                             .help("Port to listen on for DNS request (overrides the port in the config if specified).")
